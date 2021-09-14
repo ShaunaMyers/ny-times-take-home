bb67c13e6737ab7ca07b082c164d7daa301bb131
Shauna Myers <shaunamyers26@gmail.com>
2021-09-14 21:31:05 +0100

    Add route and link for specific article view

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,10 +1,14 @@
 import './Article.css';
+import { Link } from 'react-router-dom';
 
 const Article = ({ title, image }) => {
+    console.log('title', title)
     return ( 
         <article>
-            <h2>{title}</h2>
-            <img src={image}/>
+            <Link to={`/${title}`}>
+                <h2>{title}</h2>
+                <img src={image}/>
+            </Link>
         </article>
      );
 }
--- src/components/ArticleView/ArticleView.js
+++ src/components/ArticleView/ArticleView.js
@@ -1,6 +1,12 @@
-const ArticleView = ({ title, image }) => {
+const ArticleView = (props) => {
+
+    const { article = {} } = props
+
+    console.log('artice view', article)
     return ( 
         <article>
+            <h3>{article.title}</h3>
+            <img src={article.multimedia[2].url}/>
         </article>
      );
 }
