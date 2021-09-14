6278edd72265e53a31a204b715f6d437c82fee85
Shauna Myers <shaunamyers26@gmail.com>
2021-09-14 22:48:57 +0100

    Add details to ArticleView

--- src/components/ArticleView/ArticleView.js
+++ src/components/ArticleView/ArticleView.js
@@ -8,10 +8,14 @@ const ArticleView = (props) => {
     return ( 
         <article className="detailed-article-view">
             <div className="left-article-view">
-                <img src={article.multimedia[4].url}/>
+                <img src={article.multimedia[0].url}/>
             </div>
             <div className="right-article-view">
                 <h3>{article.title}</h3>
+                <p className="small-details">{article.byline}</p>
+                <p className="small-details">{article.published_date}</p>
+                <p>{article.abstract}</p>
+                <p className="small-details">Read full article <a href={article.uri}>here</a></p>
             </div>
         </article>
      );
