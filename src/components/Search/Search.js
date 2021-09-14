ef90fc87e76422f5eda28ea7f3939007aba94303
Shauna Myers <shaunamyers26@gmail.com>
2021-09-14 23:12:12 +0100

    Add state and set it onChange of input

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,15 @@
 import './Search.css';
+import { useState } from 'react';
 
 const Search = () => {
+
+    const [searchInput, setSearchInput] = useState('')
+
+
     return ( 
         <section>
             <form>
-                <input type="text"></input>
+                <input onChange={(e) => setSearchInput(e.target.value)} placeholder="Search by title or description" type="text" value={searchInput}></input>
                 <button>Search</button>
             </form>
         </section>
