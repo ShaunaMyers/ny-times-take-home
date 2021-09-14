4a17d5c4622a4076188929627b231a9a33917c67
Shauna Myers <shaunamyers26@gmail.com>
2021-09-14 18:53:58 +0100

    Pass image in props

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,16 +1,27 @@
+import './Articles.css';
 import ArticleView from "../ArticleView/ArticleView"
+// import FeatureArticle from '../FeatureArticle/FeatureArticle';
 
 
 const Articles= ({ articles }) => {
 
+    console.log('articles', articles)
+    console.log('index zero', articles[0])
+
     const allArticles = articles.map(article => {
-        return <ArticleView key={article.title} title={article.title}/>
+        return <ArticleView key={article.title} title={article.title} image={article.multimedia[2].url}/>
     })
 
+
     return ( 
-        <>
-            {allArticles}
-        </>
+        <section className="all-articles">
+            <article className="feature-article">
+                {/* <FeatureArticle article={articles[0]}/> */}
+            </article>
+            <article className="right-articles">
+                {allArticles}
+            </article>
+        </section>
      );
 }
  
