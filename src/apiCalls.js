ebbb94f5b959048df022de7beea7876ca36ab923
Shauna Myers <shaunamyers26@gmail.com>
2021-09-14 18:04:24 +0100

    Change request to fetch from home endpoint

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 export const getArticles = () => {
-    return fetch('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=aPsXlSYQF9REVR06EoCcd72NZ1H09U25')
+    return fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=aPsXlSYQF9REVR06EoCcd72NZ1H09U25')
     .then(response => {
         if (!response.ok) {
         throw Error('Oops, problem loading articles. Please refresh the page.')
