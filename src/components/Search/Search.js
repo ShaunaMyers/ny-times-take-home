1d6d413d5df1c82831fc92b690db073000576a96
Shauna Myers <shaunamyers26@gmail.com>
2021-09-14 23:09:15 +0100

    Build basic structure of search component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,14 @@
+import './Search.css';
+
+const Search = () => {
+    return ( 
+        <section>
+            <form>
+                <input type="text"></input>
+                <button>Search</button>
+            </form>
+        </section>
+     );
+}
+ 
+export default Search;
