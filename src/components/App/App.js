820c0b4f56f0d0c3032cdc052f720348d43ef34c
Shauna Myers <shaunamyers26@gmail.com>
2021-09-14 18:07:00 +0100

    Set state with sample article data & pass in props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Articles from '../Articles/Articles';
+import { getArticles } from '../../apiCalls';
+import { sampleArticles } from '../../sampleArticleData';
 
 function App() {
+
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    setArticles(sampleArticles.results)
+    // getArticles()
+    //   .then(data => setArticles(data.results))
+    //   .catch(error => console.log(error.message))
+  }, [articles])
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Your Happnins'</h1>
-        <Articles />
+        <Articles articles={articles}/>
       </header>
     </div>
   );
