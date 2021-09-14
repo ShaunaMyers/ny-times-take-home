d14c1e54e6d05898ab00c58dba8e5a7b7bdd2dd4
Shauna Myers <shaunamyers26@gmail.com>
2021-09-14 17:49:00 +0100

    Import & render Articles component in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Articles from '../Articles/Articles';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Your Happnins'</h1>
+        <Articles />
       </header>
     </div>
   );
