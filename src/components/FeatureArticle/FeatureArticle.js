7b330c94bbf675d61883950b3c50dde6afa09198
Shauna Myers <shaunamyers26@gmail.com>
2021-09-14 18:55:10 +0100

    Create FeatureArticle component files

---
+++ src/components/FeatureArticle/FeatureArticle.js
@@ -0,0 +1,10 @@
+const FeatureArticle = ({ article }) => {
+    console.log('article', article)
+    return ( 
+        <section>
+            <h2>hi</h2>
+        </section>
+     );
+}
+ 
+export default FeatureArticle;
