776e6ac88f88777eb19c23e8853f251c2f96c8bc
Shauna Myers <shaunamyers26@gmail.com>
2021-09-16 13:51:04 +0100

    Change error message

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,8 @@
 export const getArticles = () => {
-    return fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=aPsXlSYQF9REVR06EoCcd72NZ1H09U25')
+    return fetch('https://api.nytime.com/svc/topstories/v2/home.json?api-key=aPsXlSYQF9REVR06EoCcd72NZ1H09U25')
     .then(response => {
         if (!response.ok) {
-        throw Error('Oops, problem loading articles. Please refresh the page.')
+        throw Error('Problem loading articles')
         }
         return response.json()
     })
