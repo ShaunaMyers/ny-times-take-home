e70f6a596b8612b211f05f6171439683537e49ca
Shauna Myers <shaunamyers26@gmail.com>
2021-09-17 14:42:03 +0100

    Remove first article from map

--- src/components/AllArticles/AllArticles.js
+++ src/components/AllArticles/AllArticles.js
@@ -1,15 +1,17 @@
 import './AllArticles.css';
 import Article from '../Article/Article';
+import FeatureArticle from '../FeatureArticle/FeatureArticle';
 
 const AllArticles= ({ articles }) => {
 
-    const allArticles = articles.map(article => {
+    const allArticles = articles.slice(1).map(article => {
         return <Article key={article.title} title={article.title} image={article.multimedia[2].url}/>
     })
 
 
     return ( 
         <section className="all-articles">
+                <FeatureArticle article={articles[0]}/>
             <article className="right-articles">
                 {allArticles}
             </article>
