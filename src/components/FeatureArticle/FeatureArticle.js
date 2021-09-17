a541b142605c267168716e3da9ae8ca05f16f4c9
Shauna Myers <shaunamyers26@gmail.com>
2021-09-17 14:43:03 +0100

    Add className to section

--- src/components/FeatureArticle/FeatureArticle.js
+++ src/components/FeatureArticle/FeatureArticle.js
@@ -1,7 +1,9 @@
+import './FeatureArticle.css'
+
 const FeatureArticle = ({ article }) => {
     
     return ( 
-        <section>
+        <section className="feature-article-section">
             <h2>{article && article.title}</h2>
             <img src={article && article.multimedia[0].url}/>
         </section>
