a72196c4840e50b909bf427e26e6720c0cc7e1cf
Shauna Myers <shaunamyers26@gmail.com>
2021-09-17 14:56:07 +0100

    Add feature article details

--- src/components/FeatureArticle/FeatureArticle.js
+++ src/components/FeatureArticle/FeatureArticle.js
@@ -1,12 +1,28 @@
 import './FeatureArticle.css'
 
 const FeatureArticle = ({ article }) => {
+
+    const formatDate = () => {
+        const separateDate = article.published_date.split('T')
+        const splitDate = separateDate[0].split('-')
+        return `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`;
+    }
     
     return ( 
+        <>
+        {article &&
         <section className="feature-article-section">
-            <h2>{article && article.title}</h2>
-            <img src={article && article.multimedia[0].url}/>
+            <h2>{article.title}</h2>
+            <img src={article.multimedia[0].url}/>
+            <div className="full-article-view">
+                <p className="small-details">{article.byline}</p>
+                <p className="small-details">{formatDate()}</p>
+                <p>{article.abstract}</p>
+                <p className="small-details">Read full article <a href={article.url}>here</a></p>
+                </div>
         </section>
+        }
+        </>
      );
 }
  
